bd9f8fd87c155b7aaea20b2a26b57d88d92601b2
Jos33y <josephlagbalu@gmail.com>
2022-03-28 09:38:45 +0800

    testing domain sections

--- src/Shop/TrackOrder.js
+++ src/Shop/TrackOrder.js
@@ -1,4 +1,4 @@
-import {useParams} from "react-router-dom";
+
 
 const TrackOrder = () => {
     // const params = useParams()
---
+++ src/Shop/account/Login.js
@@ -0,0 +1,19 @@
+import {Col ,Container ,Row} from "react-bootstrap";
+
+const ShopLogin = () => {
+
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Col md={6}>
+
+                    </Col>
+                </Row>
+            </Container>
+        </>
+
+    )
+
+}
+export default ShopLogin
