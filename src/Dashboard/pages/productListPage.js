b8cd88f27548356387ff670dbfe18f345ef6efb3
Jos33y <josephlagbalu@gmail.com>
2022-04-08 01:58:30 +0800

    working on dashboard-4

--- src/Dashboard/pages/productListPage.js
+++ src/Dashboard/pages/productListPage.js
@@ -1,11 +1,260 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import "./pagesStyles.css"
+import {Card ,Col ,Row} from "react-bootstrap";
+import ItemOne from "../../assets/images/items/1.jpg";
+import ItemTwo from "../../assets/images/items/2.jpg";
+import ItemThree from "../../assets/images/items/3.jpg";
+import ItemFour from "../../assets/images/items/4.jpg";
+import ItemFive from "../../assets/images/items/5.jpg";
+import ItemSix from "../../assets/images/items/6.jpg";
+import ItemSeven from "../../assets/images/items/7.jpg";
+import ItemEight from "../../assets/images/items/8.jpg";
+import ItemNine from "../../assets/images/items/9.jpg";
+import ItemTen from "../../assets/images/items/10.jpg";
+
+
+
 
 const ProductListPage = () => {
     return (
         <>
-        <h5>Product List Page</h5>
-            <Link to="/dashboard/products/7899"> Details</Link>
+            <section className="content-main">
+                <div className="content-header">
+                    <h2 className="content-title"> Manage Products </h2>
+                    <div>
+                        <Link to="/dashboard/add-product" className="btn btn-primary"> Create New</Link>
+                    </div>
+                </div>
+
+                <Card className="card mb-4">
+                    <header className="card-header">
+                        <Row className="gx-3">
+                            <Col lg={4} md={6} className="me-auto">
+                                <input type="text" placeholder="Search..." className="form-control"/>
+                            </Col>
+                            <Col lg={2} md={3} className="col-6">
+                                <select className="form-select">
+                                    <option value="status">All category</option>
+                                    <option value="Active">Electronics</option>
+                                    <option value="Disabled">Clothings</option>
+                                    <option value="Show all">Others</option>
+                                </select>
+                            </Col>
+
+                            <Col lg={2} md={3} className="col-6">
+                                <select className="form-select">
+                                    <option value="show 20">Latest added</option>
+                                    <option value="show 20">Cheap first</option>
+                                    <option value="show 20">Most viewed</option>
+                                </select>
+                            </Col>
+                        </Row>
+                    </header>
+                    <div className="card-body">
+                        <Row className="gx-3 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 row-cols-xxl-5">
+                            {/*column one*/}
+                            <Col className="col">
+                                <Card className="card card-product-grid">
+                                    <Link to="https://"  className="link img-wrap">
+                                        <img src={ItemOne} alt="Product"/>
+                                    </Link>
+                                    <div className="info-wrap">
+                                        <span className="title text-truncate"> Just another product name</span>
+                                        <div className="price mb-2">&#8358; 5,000.00</div>
+                                        <Link data-bs-toggle="dropdown" to="/dashboard/add-product" className="btn btn-sm btn-outline-secondary"><i
+                                            className="fas fa-edit"></i> Edit</Link>
+                                        <Link to="/dashboard/add-product"  className="btn btn-sm btn-outline-danger ms-2"> <i
+                                            className="fas fa-trash-alt"></i> Delete </Link>
+                                    </div>
+                                </Card>
+                            </Col>
+
+                            {/*column two*/}
+                            <Col className="col">
+                                <Card className="card card-product-grid">
+                                    <Link to="https://"  className="link img-wrap">
+                                        <img src={ItemTwo} alt="Product"/>
+                                    </Link>
+                                    <div className="info-wrap">
+                                        <span className="title text-truncate"> Brow winter Jacket for Men</span>
+                                        <div className="price mb-2">&#8358; 5,000.00</div>
+                                        <Link data-bs-toggle="dropdown" to="/dashboard/add-product" className="btn btn-sm btn-outline-secondary"><i
+                                            className="fas fa-edit"></i> Edit</Link>
+                                        <Link to="/dashboard/add-product"  className="btn btn-sm btn-outline-danger ms-2"> <i
+                                            className="fas fa-trash-alt"></i> Delete </Link>
+                                    </div>
+                                </Card>
+                            </Col>
+
+                            {/*column one*/}
+                            <Col className="col">
+                                <Card className="card card-product-grid">
+                                    <Link to="https://"  className="link img-wrap">
+                                        <img src={ItemThree} alt="Product"/>
+                                    </Link>
+                                    <div className="info-wrap">
+                                        <span className="title text-truncate"> Jeans short new model</span>
+                                        <div className="price mb-2">&#8358; 5,000.00</div>
+                                        <Link data-bs-toggle="dropdown" to="/dashboard/add-product" className="btn btn-sm btn-outline-secondary"><i
+                                            className="fas fa-edit"></i> Edit</Link>
+                                        <Link to="/dashboard/add-product"  className="btn btn-sm btn-outline-danger ms-2"> <i
+                                            className="fas fa-trash-alt"></i> Delete </Link>
+                                    </div>
+                                </Card>
+                            </Col>
+
+
+                            {/*column one*/}
+                            <Col className="col">
+                                <Card className="card card-product-grid">
+                                    <Link to="https://"  className="link img-wrap">
+                                        <img src={ItemFour} alt="Product"/>
+                                    </Link>
+                                    <div className="info-wrap">
+                                        <span className="title text-truncate"> Travel Bag XL</span>
+                                        <div className="price mb-2">&#8358; 5,000.00</div>
+                                        <Link data-bs-toggle="dropdown" to="/dashboard/add-product" className="btn btn-sm btn-outline-secondary"><i
+                                            className="fas fa-edit"></i> Edit</Link>
+                                        <Link to="/dashboard/add-product"  className="btn btn-sm btn-outline-danger ms-2"> <i
+                                            className="fas fa-trash-alt"></i> Delete </Link>
+                                    </div>
+                                </Card>
+                            </Col>
+
+
+                            {/*column one*/}
+                            <Col className="col">
+                                <Card className="card card-product-grid">
+                                    <Link to="https://"  className="link img-wrap">
+                                        <img src={ItemFive} alt="Product"/>
+                                    </Link>
+                                    <div className="info-wrap">
+                                        <span className="title text-truncate"> Just another product here</span>
+                                        <div className="price mb-2">&#8358; 5,000.00</div>
+                                        <Link data-bs-toggle="dropdown" to="/dashboard/add-product" className="btn btn-sm btn-outline-secondary"><i
+                                            className="fas fa-edit"></i> Edit</Link>
+                                        <Link to="/dashboard/add-product"  className="btn btn-sm btn-outline-danger ms-2"> <i
+                                            className="fas fa-trash-alt"></i> Delete </Link>
+                                    </div>
+                                </Card>
+                            </Col>
+
+                            {/*column one*/}
+                            <Col className="col">
+                                <Card className="card card-product-grid">
+                                    <Link to="https://"  className="link img-wrap">
+                                        <img src={ItemSix} alt="Product"/>
+                                    </Link>
+                                    <div className="info-wrap">
+                                        <span className="title text-truncate"> Just another product again</span>
+                                        <div className="price mb-2">&#8358; 5,000.00</div>
+                                        <Link data-bs-toggle="dropdown" to="/dashboard/add-product" className="btn btn-sm btn-outline-secondary"><i
+                                            className="fas fa-edit"></i> Edit</Link>
+                                        <Link to="/dashboard/add-product"  className="btn btn-sm btn-outline-danger ms-2"> <i
+                                            className="fas fa-trash-alt"></i> Delete </Link>
+                                    </div>
+                                </Card>
+                            </Col>
+
+                            {/*column one*/}
+                            <Col className="col">
+                                <Card className="card card-product-grid">
+                                    <Link to="https://"  className="link img-wrap">
+                                        <img src={ItemSeven} alt="Product"/>
+                                    </Link>
+                                    <div className="info-wrap">
+                                        <span className="title text-truncate"> Just another product here again</span>
+                                        <div className="price mb-2">&#8358; 5,000.00</div>
+                                        <Link data-bs-toggle="dropdown" to="/dashboard/add-product" className="btn btn-sm btn-outline-secondary"><i
+                                            className="fas fa-edit"></i> Edit</Link>
+                                        <Link to="/dashboard/add-product"  className="btn btn-sm btn-outline-danger ms-2"> <i
+                                            className="fas fa-trash-alt"></i> Delete </Link>
+                                    </div>
+                                </Card>
+                            </Col>
+
+
+                            {/*column one*/}
+                            <Col className="col">
+                                <Card className="card card-product-grid">
+                                    <Link to="https://"  className="link img-wrap">
+                                        <img src={ItemEight} alt="Product"/>
+                                    </Link>
+                                    <div className="info-wrap">
+                                        <span className="title text-truncate"> Just another product eight</span>
+                                        <div className="price mb-2">&#8358; 5,000.00</div>
+                                        <Link data-bs-toggle="dropdown" to="/dashboard/add-product" className="btn btn-sm btn-outline-secondary"><i
+                                            className="fas fa-edit"></i> Edit</Link>
+                                        <Link to="/dashboard/add-product"  className="btn btn-sm btn-outline-danger ms-2"> <i
+                                            className="fas fa-trash-alt"></i> Delete </Link>
+                                    </div>
+                                </Card>
+                            </Col>
+
+
+
+                            {/*column one*/}
+                            <Col className="col">
+                                <Card className="card card-product-grid">
+                                    <Link to="https://"  className="link img-wrap">
+                                        <img src={ItemNine} alt="Product"/>
+                                    </Link>
+                                    <div className="info-wrap">
+                                        <span className="title text-truncate"> Just another product name</span>
+                                        <div className="price mb-2">&#8358; 5,000.00</div>
+                                        <Link data-bs-toggle="dropdown" to="/dashboard/add-product" className="btn btn-sm btn-outline-secondary"><i
+                                            className="fas fa-edit"></i> Edit</Link>
+                                        <Link to="/dashboard/add-product"  className="btn btn-sm btn-outline-danger ms-2"> <i
+                                            className="fas fa-trash-alt"></i> Delete </Link>
+                                    </div>
+                                </Card>
+                            </Col>
+
+
+
+                            {/*column one*/}
+                            <Col className="col">
+                                <Card className="card card-product-grid">
+                                    <Link to="https://"  className="link img-wrap">
+                                        <img src={ItemTen} alt="Product"/>
+                                    </Link>
+                                    <div className="info-wrap">
+                                        <span className="title text-truncate"> Just another product name</span>
+                                        <div className="price mb-2">&#8358; 5,000.00</div>
+                                        <Link data-bs-toggle="dropdown" to="/dashboard/add-product" className="btn btn-sm btn-outline-secondary"><i
+                                            className="fas fa-edit"></i> Edit</Link>
+                                        <Link to="/dashboard/add-product"  className="btn btn-sm btn-outline-danger ms-2"> <i
+                                            className="fas fa-trash-alt"></i> Delete </Link>
+                                    </div>
+                                </Card>
+                            </Col>
+                        </Row>
+
+                        <nav className="float-end mt-4" aria-label="Page navigation">
+                            <ul className="pagination">
+                                <li className="page-item disabled">
+                                    <Link to="https://" className="page-link">Previous</Link>
+                                </li>
+                                <li className="page-item active">
+                                    <Link to="https://" className="page-link">1</Link>
+                                </li>
+                                <li className="page-item">
+                                    <Link to="https://" className="page-link">2</Link>
+                                </li>
+                                <li className="page-item">
+                                    <Link to="https://" className="page-link">3</Link>
+                                </li>
+                                <li className="page-item disabled">
+                                    <Link to="https://" className="page-link">Next</Link>
+                                </li>
+                            </ul>
+                        </nav>
+
+                    </div>
+                </Card>
+            </section>
+            {/*<Link to="/dashboard/products/7899"> Details</Link>*/}
         </>
     )
 
--- src/Dashboard/pages/settingsPage.js
+++ src/Dashboard/pages/settingsPage.js
@@ -1,9 +1,118 @@
 import React from "react";
+import "./pagesStyles.css"
+import {Button ,Card ,Col ,Form ,Row} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import AvatarOne from "../../assets/images/sellers/avatar1.jpg";
 
 const SettingsPage = () => {
     return (
         <>
-            <h6>Settings Page</h6>
+            <section className="content-main">
+                <div className="content-header">
+                    <h2 className="content-title"> Profile setting </h2>
+                </div>
+
+                <Card className="card">
+                    <div className="card-body">
+                        <Row className="gx-5">
+                            <aside className="col-lg-3 border-end">
+                                <nav className="nav nav-pills flex-lg-column mb-4">
+                                    <Link  to="https://" aria-current="page" className="nav-link active"> General</Link>
+                                    <Link  to="https://" className="nav-link"> Moderators</Link>
+                                    <Link  to="https://" className="nav-link"> Admin account</Link>
+                                    <Link  to="https://" className="nav-link"> SEO settings</Link>
+                                    <Link  to="https://" className="nav-link"> Mail Settings</Link>
+                                    <Link  to="https://" className="nav-link"> Newsletter</Link>
+                                </nav>
+                            </aside>
+
+                            <Col lg={9}>
+                                <section className="content-body p-xl-4">
+                                    <Form>
+                                        <Row>
+                                            <Col lg={8}>
+                                                <Row className="gx-3">
+                                                    <div className="col-6 mb-3">
+                                                        <label htmlFor="firstname" className="form-label">First Name </label>
+                                                        <input type="text" placeholder="Type here..." className="form-control"/>
+                                                    </div>
+
+                                                    <div className="col-6 mb-3">
+                                                        <label htmlFor="lastname" className="form-label">Last Name </label>
+                                                        <input type="text" placeholder="Type here..." className="form-control"/>
+                                                    </div>
+
+                                                    <div className="col-6 mb-3">
+                                                        <label htmlFor="businessname" className="form-label">Business Name </label>
+                                                        <input type="text" placeholder="Type here..." className="form-control"/>
+                                                    </div>
+
+                                                    <div className="col-6 mb-3">
+                                                        <label htmlFor="storename" className="form-label">Store Url </label>
+                                                        <input type="text" placeholder="Type here..." className="form-control"/>
+                                                    </div>
+
+                                                    <div className="col-lg-6 mb-3">
+                                                        <label htmlFor="email" className="form-label">Email </label>
+                                                        <input type="email" placeholder="Type here..." className="form-control"/>
+                                                    </div>
+
+                                                    <div className="col-lg-6 mb-3">
+                                                        <label htmlFor="phone" className="form-label">Phone </label>
+                                                        <input type="tel" placeholder="Type here..." className="form-control"/>
+                                                    </div>
+
+                                                    <div className="col-lg-12 mb-3">
+                                                        <label htmlFor="address" className="form-label">Address </label>
+                                                        <input type="text" placeholder="Type here..." className="form-control"/>
+                                                    </div>
+
+                                                    <div className="col-lg-6 mb-3">
+                                                        <label htmlFor="birthday" className="form-label">Birthday </label>
+                                                        <input type="date" className="form-control"/>
+                                                    </div>
+                                                </Row>
+                                            </Col>
+                                            <aside className="col-lg-4">
+                                                <figure className="text-lg-center">
+                                                    <img src={AvatarOne} alt="user avatar" className="img-lg mb-3 img-avatar"/>
+                                                    <figcaption>
+                                                        <Link to="https://" className="btn btn-outline-primary">
+                                                            <i className="fas fa-upload"></i> upload
+                                                        </Link>
+                                                    </figcaption>
+                                                </figure>
+                                            </aside>
+                                        </Row>
+                                        <br/>
+                                        <Button  className="btn btn-primary" type="submit"> Save changes</Button>
+                                    </Form>
+
+                                    <hr className="my-5"/>
+                                    <Row style={{maxWidth: '920px'}}>
+                                        <div className="col-md">
+                                            <article className="box mb-3 bg-light">
+                                                <Link to="https://" className="btn float-end btn-outline-secondary btn-sm">Change</Link>
+                                                <h6>Password</h6>
+                                                <small className="text-muted d-block" style={{width: '70%'}}>You can reset or change your password by clicking here</small>
+                                            </article>
+                                        </div>
+
+                                        <div className="col-md">
+                                            <article className="box mb-3 bg-light">
+                                                <Link to="https://" className="btn float-end btn-outline-danger btn-sm">Deactivate</Link>
+                                                <h6>Remove account</h6>
+                                                <small className="text-muted d-block" style={{width: '70%'}}>Once you delete your account, there is no going back.</small>
+                                            </article>
+                                        </div>
+                                    </Row>
+                                </section>
+
+                            </Col>
+                        </Row>
+                    </div>
+                </Card>
+            </section>
         </>
     )
 
